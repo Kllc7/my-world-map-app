22fe7231dd24e279236b7db5d2afbfdc5f05fe9d
Khalil Chahi <khalilchahi191919@gmail.com>
2025-05-10 05:29:34 +0100

    Fix vite.config.js for base URL

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default {
-  base: '/my-world-map-app/',
-}
+export default defineConfig({
+  plugins: [react()],
+  base: '/my-world-map-app/', // Set the base URL for your app
+})
